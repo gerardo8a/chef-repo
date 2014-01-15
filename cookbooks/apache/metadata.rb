4759bb7cfb2a07e7048057fef772086ae49eada7
Gerardo Porras <gporras@yahoo-inc.com>
2014-01-15 11:00:06 -0800

    Latest

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -1,7 +1,7 @@
 name             'apache'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Yahoo!'
+maintainer_email 'gporras@yahoo-inc.com'
 license          'All rights reserved'
 description      'Installs/Configures apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.1'
--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -10,11 +10,56 @@ package "apache2" do
   action :install
 end
 
+node['apache']['sites'].each do | site_name, site_data |
+  document_root = "/srv/apache/#{site_name}"
+  template "/etc/apache2/sites-available/#{site_name}" do
+    source "custom.erb"
+    mode "0644"
+    variables(
+      :document_root => document_root,
+      :port => site_data['port']
+    )
+    notifies :restart, "service[apache2]"
+  end
+
+  execute "a2ensite #{site_name}" do
+    not_if do
+      ::File.symlink?("/etc/apache2/sites-enabled/#{site_name}")
+    end
+    notifies :restart, "service[apache2]"
+  end
+
+  directory document_root do
+    mode "0755"
+    recursive true
+  end
+
+  template "#{document_root}/index.html" do
+    source "index.html.erb"
+    mode "0644"
+    variables(
+      :site_name => site_name,
+      :port => site_data['port']
+    )
+  end
+end
+
 service "apache2" do
   action [:enable, :start]
 end
 
-cookbook_file "/var/www/index.html" do
-	source "index.html"
-	mode "0644"
+execute "a2dissite default" do
+	only_if do
+	  ::File.symlink?("/etc/apache2/sites-enabled/000-default")
+	end
+	notifies :restart, "service[apache2]"
 end
+
+#https://gist.github.com/2866378
+### Use a directory resource
+  # The name is document_root
+  # The resource has two parameters
+  #   mode is "0755"
+  #   recursive is true
+  # Use the Resources page on the Docs Site to read more about what recursive does
+  #
